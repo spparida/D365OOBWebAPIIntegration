67013503346f26d09138e7bb3408a568f9ce8b86
Satya Prakash Parida <satprpa@microsoft.com>
2019-03-10 19:22:26 +0530

    Fisrt Commit
    Sample Application to connect to D365 using XRM Toolkit & OAuth 2

---
+++ UsingOAuth/Program.cs
@@ -0,0 +1,62 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using System.Net;
+using System;
+using System.Threading.Tasks;
+
+namespace UsingOAuth
+{
+    class Program
+    {
+        // O365 User Name and Password
+        private const string userName = "satya@bizappsmar19.onmicrosoft.com";
+        private const string password = "";
+        // D365 Application Url 
+        private const string serviceUrl = "https://bizappsmar19.api.crm8.dynamics.com";
+        // Azure APP Application Id
+        private const string applicationId = "xxxxxxx-6051-415e-8729-c690a151e7dd";
+        // Redirct Uri specified during registration of application
+        private const string RedirectUri = "https://localhost";
+        // OAuth 2.0 Authorization Endpoint copied from Azure APP
+        private const string authorityUri = "https://login.microsoftonline.com/xxxx-a189-4ee4-908e-fd6b9a467e7d/oauth2/authorize";
+        private static AuthenticationResult authResult = null;
+        static void Main(string[] args)
+        {
+            // Code to connect to D365
+            var credentials = new UserPasswordCredential(userName, password);
+            var context = new AuthenticationContext(authorityUri);
+            authResult = context.AcquireTokenAsync(serviceUrl, applicationId, credentials).Result;
+             Task.WaitAll(Task.Run(async () => await ExecuteWhoAmI()));
+            
+        }
+
+        private static async Task ExecuteWhoAmI()
+        {
+            var httpClient = new HttpClient
+            {
+                BaseAddress = new Uri(serviceUrl),
+                Timeout = new TimeSpan(0, 2, 0)
+            };
+            httpClient.DefaultRequestHeaders.Add("OData-MaxVersion", "4.0");
+            httpClient.DefaultRequestHeaders.Add("OData-Version", "4.0");
+            httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", authResult.AccessToken);
+            // Add this line for TLS complaience
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls11 | SecurityProtocolType.Tls12;
+            // Call WhoAmI
+            var retrieveResponse = await httpClient.GetAsync("api/data/v9.1/WhoAmI");
+            if (retrieveResponse.IsSuccessStatusCode)
+            {
+                var jRetrieveResponse = JObject.Parse(retrieveResponse.Content.ReadAsStringAsync().Result);
+                var currUserId = (Guid)jRetrieveResponse["UserId"];
+                var businessId = (Guid)jRetrieveResponse["BusinessUnitId"];
+                Console.WriteLine("My User Id – " + currUserId);
+                Console.WriteLine("My User Id – " + businessId);
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ UsingXRMToolKit/Program.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Client;
+using System.Configuration;
+using Microsoft.Xrm.Tooling.Connector;
+using Microsoft.Crm.Sdk.Messages;
+using System.Net;
+using System;
+
+namespace UsingXRMToolKit
+{
+    class Program
+    {
+        private static OrganizationServiceProxy _serviceProxy;
+
+        private static IOrganizationService _service;
+        static void Main(string[] args)
+        {
+            /*var connectionString = ConfigurationManager.ConnectionStrings["Xrm"].ConnectionString;
+            var crmConn = new CrmServiceClient(connectionString);
+
+            using (_serviceProxy = crmConn.OrganizationServiceProxy)
+            {
+
+                _service = _serviceProxy;
+
+                var reqWhoAmI = new WhoAmIRequest();
+
+                var resp = (WhoAmIResponse)_service.Execute(reqWhoAmI);
+
+                var buID = resp.OrganizationId.ToString();
+
+                var userID = resp.UserId.ToString();
+            }*/
+
+            string connectionString = ConfigurationManager.ConnectionStrings["Xrm"].ConnectionString;
+            CrmServiceClient conn = new CrmServiceClient(connectionString);
+
+            OrganizationServiceProxy orgService = conn.OrganizationServiceProxy;
+
+            // To impersonate, set the caller GUID
+            orgService.CallerId = new Guid("67ab9cb6-96c3-4849-8cab-6c2135793f06");
+
+            // Create Account
+
+            Entity account = new Entity("account");
+            account["name"] = "Satya Parida";
+            Guid accountId = orgService.Create(account);
+
+            // Who Am I Request
+
+            // Guid userid = ((WhoAmIResponse)orgService.Execute(new WhoAmIRequest())).UserId;
+
+            Console.ReadLine();
+        }
+
+    }
+
+    // <summary>
+    /// Insert records to Dynamics Entity
+    /// </summary>
+    /// <param name="result"></param>
+    /// <returns></returns>
+    /*public string PostToCrm()
+    {
+        ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+
+        CrmServiceClient service = new CrmServiceClient(ConfigurationManager.ConnectionStrings["CRMConnection"].ConnectionString);
+        Entity leadRecord = new Entity("new_businesscardlead");
+
+
+        leadRecord.Attributes.Add("new_name", "");
+        leadRecord.Attributes.Add("new_company", "");
+        leadRecord.Attributes.Add("new_title", "");
+        leadRecord.Attributes.Add("new_citystatezip", "");
+        leadRecord.Attributes.Add("new_email", "");
+        leadRecord.Attributes.Add("new_website", "");
+        leadRecord.Attributes.Add("new_facebook", "");
+        leadRecord.Attributes.Add("new_twitter", "");
+        leadRecord.Attributes.Add("new_phone", "");
+        leadRecord.Attributes.Add("new_fax", "");
+        leadRecord.Attributes.Add("new_cell", "");
+
+        var guid = service.Create(leadRecord);
+        string id = guid.ToString();
+        string response = $"Lead has been created for: lead Id : {id}.";
+        return response;
+    }*/
+}
